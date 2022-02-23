ca792ebf76d5621a24bf3d799f52b8a93bfb6a2d
Jan Chabik <jan.chabik@softwareplant.com>
2022-02-23 10:34:32 +0100

    Added ExceptionHandling

---
+++ src/main/java/com/janchabik/gameservice/rest/IllegalArgumentExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.janchabik.gameservice.rest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class IllegalArgumentExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Object> handleIllegalArgumentException(
+			IllegalArgumentException illegalArgumentException
+	) {
+		Map<String, Object> body = new HashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", illegalArgumentException.getMessage());
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+}
